a4d26b238066b2403c764585bd95c51b067303af
Kouzola <pierrehenry.lesage@gmail.com>
2025-02-27 20:33:04 +0100

    Service + Repo  DTO Interface StudentGroup

---
+++ Backend/Calempus360.Core/Interfaces/Group/IStudentGroupRepository.cs
@@ -0,0 +1,18 @@
+﻿using Calempus360.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calempus360.Core.Interfaces.Group
+{
+    public interface IStudentGroupRepository
+    {
+        Task<IEnumerable<StudentGroup>> GetAllStudentGroupAsync();
+        Task<StudentGroup> GetStudentGroupAsyncById(int id);
+        Task AddStudentGroupAsync(StudentGroup studentGroup);
+        Task<bool> UpdateStudentGroupAsync(StudentGroup studentGroup);
+        Task<bool> DeleteStudentGroupAsyncById(int id);
+    }
+}
---
+++ Frontend/Calempus360.Admin/src/app/features/student-groups/student-groups.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-groups',
+  imports: [],
+  templateUrl: './student-groups.component.html',
+  styleUrl: './student-groups.component.scss'
+})
+export class StudentGroupsComponent {
+
+}
