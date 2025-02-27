d8482053059a893cae023fb58bc51a871c5e172d
Kouzola <pierrehenry.lesage@gmail.com>
2025-02-27 22:28:45 +0100

    repo StudentGroup getAllMethod

---
+++ Backend/Calempus360.Infrastructure/Repositories/StudentGroupRepository.cs
@@ -0,0 +1,50 @@
+﻿using Calempus360.Core.Interfaces.Group;
+using Calempus360.Core.Models;
+using Calempus360.Infrastructure.Data;
+using Calempus360.Infrastructure.Persistence.Mappers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calempus360.Infrastructure.Repositories
+{
+    public class StudentGroupRepository : IStudentGroupRepository
+    {
+        private readonly Calempus360DbContext _context;
+
+        public StudentGroupRepository(Calempus360DbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddStudentGroupAsync(StudentGroup studentGroup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteStudentGroupAsyncById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<StudentGroup>> GetAllStudentGroupAsync()
+        {
+            var entities = await _context.StudentGroups.ToListAsync();
+
+            return entities.Select(e => e.ToDomainModel());
+        }
+
+        public async Task<StudentGroup> GetStudentGroupAsyncById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateStudentGroupAsync(StudentGroup studentGroup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
