b0e2c331fa82172151d75865aa27268c6312e6d5
Kouzola <pierrehenry.lesage@gmail.com>
2025-02-27 23:49:20 +0100

    guid

--- Backend/Calempus360.Core/DTOs/Responses/GetStudentGroupResponse.cs
+++ Backend/Calempus360.Core/DTOs/Responses/GetStudentGroupResponse.cs
@@ -8,7 +8,6 @@ namespace Calempus360.Core.DTOs.Responses
 {
     public class GetStudentGroupResponse
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Code {  get; set; } = string.Empty;
         public string OptionGrade {  get; set; } = string.Empty;
