d330878a9089c43a4ea289e1f6d744ce830988d2
Kouzola <pierrehenry.lesage@gmail.com>
2025-03-02 00:14:40 +0100

    StudentGroupService + quelques exception

---
+++ Backend/Calempus360.Errors/StudentGroup/StudentGroupNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calempus360.Errors.StudentGroup
+{
+    public class StudentGroupNotFoundException(Guid id) : Exception($"Student Group with Id : {id} not found !")
+    {
+
+    }
+}
