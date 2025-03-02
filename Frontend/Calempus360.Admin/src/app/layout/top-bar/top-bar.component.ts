622aa1cdce885795ba3488c35cde141aa1007534
fratjo <francois.tjoens.pro@gmail.com>
2025-03-02 22:56:13 +0100

    Config + Test DaisyUI for better UI

--- Frontend/Calempus360.Admin/src/app/layout/top-bar/top-bar.component.ts
+++ Frontend/Calempus360.Admin/src/app/layout/top-bar/top-bar.component.ts
@@ -1,19 +1,13 @@
-import {
-  Component,
-  inject,
-  input,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { UniversityService } from '../../core/services/university.service';
 import { AsyncPipe } from '@angular/common';
 import { AcademicYearService } from '../../core/services/academic-year.service';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [MatToolbarModule, AsyncPipe],
+  imports: [MatToolbarModule, AsyncPipe, RouterModule, RouterLink],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
