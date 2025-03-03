2177d8440bf32b3edb79fafc7c8010c5a10dd188
Kouzola <pierrehenry.lesage@gmail.com>
2025-03-03 01:17:53 +0100

    Controller + Repo + Service Ajout

--- Backend/Calempus360.Infrastructure/Repositories/StudentGroupRepository.cs
+++ Backend/Calempus360.Infrastructure/Repositories/StudentGroupRepository.cs
@@ -15,7 +15,7 @@ namespace Calempus360.Infrastructure.Repositories
     public class StudentGroupRepository : IStudentGroupRepository
     {
         private readonly Calempus360DbContext _context;
-
+        //TODO : Ajout Include dans Update et GetStudent
         public StudentGroupRepository(Calempus360DbContext context)
         {
             _context = context;
@@ -41,27 +41,59 @@ namespace Calempus360.Infrastructure.Repositories
 
         public async Task<IEnumerable<StudentGroup>> GetAllStudentGroupAsync()
         {
-            var entities = await _context.StudentGroups.ToListAsync();
+            var entities = await _context.StudentGroups
+                .Include(sg => sg.SiteEntity)
+                .Include(sg => sg.AcademicYearEntity)
+                .Include(sg => sg.OptionEntity)
+                .ToListAsync();
 
             return entities.Select(e => e.ToDomainModel());
         }
 
         public async Task<StudentGroup?> GetStudentGroupByIdAsync(Guid id)
         {
-            var entity = await _context.StudentGroups.FindAsync(id);
+            var entity = await _context.StudentGroups
+                .Include(sg => sg.SiteEntity)
+                .Include(sg => sg.AcademicYearEntity)
+                .Include(sg => sg.OptionEntity)
+                .FirstOrDefaultAsync(sg => sg.StudentGroupId == id);
             return entity?.ToDomainModel();
         }
 
-        public async Task<bool> UpdateStudentGroupAsync(StudentGroup studentGroup)
+        public async Task<bool> UpdateStudentGroupAsync(StudentGroup studentGroup, Guid id)
         {
             var updatedEntity = studentGroup.ToEntity();
-            var entity = await _context.StudentGroups.FindAsync(updatedEntity.StudentGroupId);
+            var entity = await _context.StudentGroups
+                .Include(sg => sg.SiteEntity)
+                .Include(sg => sg.AcademicYearEntity)
+                .Include(sg => sg.OptionEntity)
+                .FirstOrDefaultAsync(sg => sg.StudentGroupId == studentGroup.Id);
             if(entity != null)
             {
-                entity = updatedEntity;
+                entity.Code = studentGroup.Code;
+                entity.NumberOfStudents = studentGroup.NumberOfStudents;
+                entity.OptionGrade = studentGroup.OptionGrade;
+                entity.SiteId = studentGroup.Site.Id;
+                entity.OptionId = studentGroup.Option.Id;
+
                 return await _context.SaveChangesAsync() > 0;
             }
             return false;
+        }
+
+        //----------Méthode pour certains tests
+
+        //Trouver site selon nom
+        public async Task<Site> GetSiteByName(string name)
+        {
+            var site = await _context.Sites.FirstOrDefaultAsync(s => s.Name == name);
+            return site.ToDomainModel();
+        }
+        //Trouver option selon nom
+        public async Task<Option> GetOptionByName(string name)
+        {
+            var option = await _context.Options.FirstOrDefaultAsync(s => s.Name == name);
+            return option.ToDomainModel();
         }
     }
 }
