f7ab1288a05e8d99b2827a3bd5bebdc5b8220625
fratjo <francois.tjoens.pro@gmail.com>
2025-03-26 15:40:34 +0100

    Play with calendar

--- Frontend/Calempus360.Admin/src/app/features/session/session.component.ts
+++ Frontend/Calempus360.Admin/src/app/features/session/session.component.ts
@@ -3,6 +3,7 @@ import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core/index.js';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-session',
@@ -12,16 +13,80 @@ import interactionPlugin from '@fullcalendar/interaction';
 })
 export class SessionComponent {
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin, interactionPlugin],
-    dateClick: (arg) => this.handleDateClick(arg),
+    initialView: 'timeGridWeek',
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'today,timeGridWeek,timeGridDay,dayGridMonth', // user can switch between the two
+    },
+    views: {
+      dayGridMonth: {
+        titleFormat: { year: 'numeric', month: 'long' },
+      },
+      timeGrid: {
+        titleFormat: { year: 'numeric', month: 'long', day: 'numeric' },
+      },
+    },
+    nowIndicator: true,
+    weekends: false,
+    editable: true,
+    selectable: true,
     events: [
-      { title: 'event 1', date: '2025-04-01' },
+      {
+        // this object will be "parsed" into an Event Object
+        title: 'The Title', // a property!
+        start: '2025-03-26T10:00:00', // a property!
+        end: '2025-03-27T10:00:00', // a property! ** see important note below about 'end' **
+      },
+      {
+        title: 'event 1',
+        start: '2025-04-01',
+        end: '2025-04-01',
+      },
       { title: 'event 2', date: '2025-04-02' },
     ],
+
+    eventClick(arg) {
+      alert('Event: ' + arg.event.title);
+    },
+
+    eventDragStart(arg) {
+      console.log('eventDragStart', arg.event.start);
+    },
+
+    eventDragStop(arg) {
+      console.log('eventDragStop', arg.event.start);
+    },
+
+    eventDrop: function (info) {
+      console.log(info.event.start);
+
+      alert(
+        info.event.title +
+          ' was dropped on ' +
+          (info.event.start ? info.event.start : 'an unknown date'),
+      );
+
+      if (!confirm('Are you sure about this change?')) {
+        info.revert();
+      }
+    },
+
+    eventResizeStart(arg) {
+      console.log('eventResizeStart', arg.event.start);
+    },
+
+    eventResizeStop(arg) {
+      console.log('eventResizeStop', arg.event.start);
+    },
   };
 
   handleDateClick(arg: any) {
     alert('date click! ' + arg.dateStr);
+  }
+
+  toggleWeekends() {
+    this.calendarOptions.weekends = !this.calendarOptions.weekends; // toggle the boolean!
   }
 }
